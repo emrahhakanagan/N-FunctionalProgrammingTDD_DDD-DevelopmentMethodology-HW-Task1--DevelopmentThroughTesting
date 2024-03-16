91a989a93cf68e738d27e0c9882c268e4282ab3f
emrahhakan <emrahhakanagan@gmail.com>
2024-03-16 21:23:43 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    private final Map<String, String> contacts = new HashMap<>();
+
+    public int add(String name, String number) {
+        return 0;
+    }
+// add method implemented
+
 
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,5 +1,36 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class PhoneBookTest {
+
+    // Initial commit
+
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    void setup() {
+        phoneBook = new PhoneBook();
+    }
+
+    // Тесты для добавления метода нового контакта
+    @Test
+    public void test_AddContact_ShouldReturn_True() {
+        var actual = phoneBook.add("Alice", "123-456-789");
+
+        Assert.assertEquals("Stub and tests for add", 1, actual);
+    }
+
+
+    @Test
+    public void test_AddContact_ShouldReturn_False() {
+        phoneBook.add("Alice", "123-456-789");
+
+        var actual = phoneBook.add("Alice", "123-456-789");
+
+        Assert.assertEquals("Stub and tests for add", 1, actual);
+    }
 
 }
